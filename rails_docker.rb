79aae40e5ba4535366a815c20105ee5e283dc228
Carlos Herrera <caherrerapa@gmail.com>
2017-03-02 15:46:35 +0700

    Fixing paths

--- rails_docker.rb
+++ rails_docker.rb
@@ -39,12 +39,13 @@ gsub_file 'docker-compose.yml', '#{app_name}', "#{app_name}"
 remove_file '.dockerignore'
 copy_file 'rails_docker/.dockerignore', '.dockerignore'
 
-copy_file 'rails_docker/application.yml', 'app/config/application.yml'
+remove_file 'config/application.yml'
+copy_file 'rails_docker/application.yml', 'config/application.yml'
 gsub_file 'config/application.yml', '#{app_name}', "#{app_name}"
 
 # Database.yml
-remove_file 'config/database.yml'
-copy_file 'rails_docker/database.yml', 'app/config/database.yml'
+remove_file 'app/config/database.yml'
+copy_file 'rails_docker/database.yml', 'config/database.yml'
 gsub_file 'config/database.yml', '#{app_name}', "#{app_name}"
 
 # Removing turbolinks
