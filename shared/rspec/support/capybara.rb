f65974303ecb6081434b653206721457d9809642
Olivier Robert <olivierobert@gmail.com>
2018-02-01 16:14:17 +0700

    Update the Capybara configuration for :headless_chrome to work in a Docker container

--- shared/rspec/support/capybara.rb
+++ shared/rspec/support/capybara.rb
@@ -1,7 +1,18 @@
+require 'selenium-webdriver'
+require 'capybara'
+
 # https://github.com/rails/rails/pull/30876
 Capybara.register_driver(:headless_chrome) do |app|
+  # no-sandbox
+  #   Because the user namespace is not enabled in the container by default
+  # headless
+  #   Runs without actually launching gui
+  # disable-gpu
+  #   Disables graphics processing unit(GPU) hardware acceleration
+  # window-size
+  #   sets default window size in case the smaller default size is not enough
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu window-size=1280,720] }
+    chromeOptions: { args: %w[no-sandbox headless disable-gpu window-size=1280,720] }
   )
 
   Capybara::Selenium::Driver.new(
