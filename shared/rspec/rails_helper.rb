3f1dff298d50b0b3e1260cc45d8a1b04c8b75373
Somsak Arnon <lahphim@gmail.com>
2018-05-03 11:01:57 +0700

    Add some comment

--- shared/rspec/rails_helper.rb
+++ shared/rspec/rails_helper.rb
@@ -13,6 +13,7 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.fixture_path = Rails.root.join('spec', 'fabricators')
 
+  # Set `true` for using in System test
   config.use_transactional_fixtures = true
 
   config.infer_spec_type_from_file_location!
