511318273962f2224d00404aa55664c6ffceab14
Rossukhon Leagmongkol <rose.roko@gmail.com>
2019-04-22 15:32:11 +0700

    Include the i18n and translations in the layout instead of the asset pipeline

--- lib/i18n.rb
+++ lib/i18n.rb
@@ -1,12 +1,10 @@
 def setup_rails_i18n
   inject_into_class 'app/controllers/application_controller.rb', 'ApplicationController' do
-    "  include Localization\n"
+    "\tinclude Localization\n"
   end
 end
 
 def setup_i18n_js
-  gsub_file 'app/views/layouts/application.html.erb', /<html>/, "<html lang='<%= I18n.locale %>'>"
-
   environment(nil, env: 'development') do
     <<~EOT
       # Automatically generate the `translation.js` files
@@ -15,11 +13,19 @@ def setup_i18n_js
     EOT
   end
 
-  insert_into_file 'app/assets/javascripts/application.js', after: "//= require activestorage\n" do
+  gsub_file 'app/views/layouts/application.html.erb', /<html>/ do
+    "<html lang='<%= I18n.locale %>'>"
+  end
+
+  insert_into_file 'app/views/layouts/application.html.erb', after: /<%= stylesheet_link_tag.*\n/ do
     <<~EOT
-      //= require i18n
-      //= require translations/translations
+      \t\t<%= javascript_include_tag 'i18n' %>
+      \t\t<%= javascript_include_tag 'translations/translations' %>
     EOT
+  end
+
+  append_to_file 'config/initializers/assets.rb' do
+    "Rails.application.config.assets.precompile += %w( i18n.js translations/translations.js )\n"
   end
 
   append_to_file '.gitignore' do
