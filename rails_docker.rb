8888bca6af87d019cd0f465abafb7370ace6ae40
Rossukhon Leagmongkol <rose.roko@gmail.com>
2019-05-16 16:28:25 +0700

    Update Rails to 6.0.0.rc1

--- rails_docker.rb
+++ rails_docker.rb
@@ -77,15 +77,16 @@ setup_config
 
 # Setup Javascript and Stylesheets
 # Remove Turbolinks from `application.js` file
-gsub_file 'app/assets/javascripts/application.js', %r{^\/\/= require turbolinks\n}, ''
-directory 'shared/app/assets/javascripts', 'app/assets/javascripts'
-gsub_file('app/assets/javascripts/application.js', %r{\/\/= require_tree .\n}, '')
-insert_into_file 'app/assets/javascripts/application.js', after: "//= require activestorage\n" do
-  <<~EOT
-    //= require initializers/index
-    //= require screens/index
-  EOT
-end
+# TODO: Recheck the Rails 6 update
+# gsub_file 'app/assets/javascripts/application.js', %r{^\/\/= require turbolinks\n}, ''
+# directory 'shared/app/assets/javascripts', 'app/assets/javascripts'
+# gsub_file('app/assets/javascripts/application.js', %r{\/\/= require_tree .\n}, '')
+# insert_into_file 'app/assets/javascripts/application.js', after: "//= require activestorage\n" do
+#   <<~EOT
+#     //= require initializers/index
+#     //= require screens/index
+#   EOT
+# end
 
 remove_file 'app/assets/stylesheets/application.css'
 directory 'shared/app/assets/stylesheets', 'app/assets/stylesheets'
