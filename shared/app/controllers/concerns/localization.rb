820283757b82dbfa072232e4ce0ade1c91ff52f0
An <duonghienan.dha@gmail.com>
2019-11-21 09:50:50 +0700

    Adding default_url_options

--- shared/app/controllers/concerns/localization.rb
+++ shared/app/controllers/concerns/localization.rb
@@ -9,5 +9,11 @@ module Localization
     def set_locale
       I18n.locale = params[:locale] || I18n.default_locale
     end
+
+    private
+
+    def default_url_options
+      { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+    end
   end
 end
