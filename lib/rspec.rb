67010258cb13f3d73742cbbcd8f17a1db236ad6a
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-02-07 10:39:27 +0700

    Fix the rspec folder creation

--- lib/rspec.rb
+++ lib/rspec.rb
@@ -3,13 +3,13 @@ def setup_rspec
   remove_file ".rspec"
   copy_file 'shared/rspec/.rspec', '.rspec'
 
-  FileUtils.cp_r "#{current_directory}/shared/rspec/.", 'spec/'
-  run 'rm -rf spec/.rspec' # remove the .rspec file from rspec folder
-
   # folder for rspec
   run 'mkdir spec/codebase'
   run 'mkdir spec/fabricators'
   run 'mkdir spec/system'
   run 'mkdir spec/requests'
   run 'mkdir spec/support'
+
+  FileUtils.cp_r "#{current_directory}/shared/rspec/.", 'spec/'
+  run 'rm -rf spec/.rspec' # remove the .rspec file from rspec folder
 end
