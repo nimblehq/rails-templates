b93a4daa06f935c2224f44f812455fa00e96b76c
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-02-19 09:54:54 +0700

    Remove file name variables

--- variants/web/Gemfile.rb
+++ variants/web/Gemfile.rb
@@ -1,12 +1,10 @@
-GEMFILE = 'Gemfile'
-
-insert_into_file GEMFILE, after: %r{gem 'bootsnap'.*\n} do
+insert_into_file 'Gemfile', after: %r{gem 'bootsnap'.*\n} do
   <<~EOT
     gem 'i18n-js' # A library to provide the I18n translations on the Javascript
   EOT
 end
 
-insert_into_file GEMFILE, after: %r{gem 'pundit'.*\n} do
+insert_into_file 'Gemfile', after: %r{gem 'pundit'.*\n} do
   <<~EOT
   
     # Assets
@@ -19,13 +17,13 @@ end
 # Group: :development, :test
 ############################
 
-insert_into_file GEMFILE, after: %r{gem 'letter_opener'.*\n} do
+insert_into_file 'Gemfile', after: %r{gem 'letter_opener'.*\n} do
   <<-EOT
   gem 'sassc-rails' # Gem to generate scss source maps.
   EOT
 end
 
-insert_into_file GEMFILE, after: %r{gem 'pronto-reek'.*\n} do
+insert_into_file 'Gemfile', after: %r{gem 'pronto-reek'.*\n} do
   <<-EOT
   gem 'pronto-scss', require: false
   gem 'pronto-eslint_npm', require: false
@@ -36,7 +34,7 @@ end
 # Group: :test
 ##############
 
-insert_into_file GEMFILE, after: %r{gem 'rspec-retry'.*\n} do
+insert_into_file 'Gemfile', after: %r{gem 'rspec-retry'.*\n} do
   <<-EOT
   gem 'capybara', '>= 2.15' # Integration testing
   gem 'webdrivers' # Run Selenium tests more easily with automatic installation and updates for all supported webdrivers
--- variants/web/config/environments/test.rb
+++ variants/web/config/environments/test.rb
@@ -1,13 +1,11 @@
-TEST_CONFIG = 'config/environments/test.rb'
-
-insert_into_file TEST_CONFIG, before: %r{Rails.application.configure do} do
+insert_into_file 'config/environments/test.rb', before: %r{Rails.application.configure do} do
   <<~EOT
       require_relative '../../spec/support/disable_animation'
 
   EOT
 end
 
-insert_into_file TEST_CONFIG, before: %r{^end} do
+insert_into_file 'config/environments/test.rb', before: %r{^end} do
   <<-EOT
 
   # Disable all animation during tests
