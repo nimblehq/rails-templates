5c4211e206043e00d468c3377e9a9aa401a4738b
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-02-20 09:40:46 +0700

    Fix typo

--- variants/web/config/environments/test.rb
+++ variants/web/config/environments/test.rb
@@ -11,7 +11,7 @@ insert_into_file 'config/environments/test.rb', before: %r{^end} do
   # Disable all animation during tests
   config.middleware.use Rack::NoAnimations
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # Do not fallback to assets pipeline if a precompiled asset is missing.
   config.assets.compile = false
   EOT
 end
