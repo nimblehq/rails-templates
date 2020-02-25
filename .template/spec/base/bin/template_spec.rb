92d701e6f7a0099ea625c0f1044485eebcb02351
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-02-25 11:17:02 +0700

    Update bin spec

--- .template/spec/base/bin/template_spec.rb
+++ .template/spec/base/bin/template_spec.rb
@@ -1,18 +1,16 @@
 describe '/bin template' do
-  EXECUTABLE_MODE = 755
-
   it 'creates the envsetup script' do
     expect(file('bin/envsetup.sh')).to exist
-    expect(file('bin/envsetup.sh')).to be_mode(EXECUTABLE_MODE)
+    expect(file('bin/envsetup.sh')).to be_executable
   end
 
   it 'creates the start script' do
     expect(file('bin/start.sh')).to exist
-    expect(file('bin/start.sh')).to be_mode(EXECUTABLE_MODE)
+    expect(file('bin/start.sh')).to be_executable
   end
 
   it 'creates the test script' do
     expect(file('bin/test.sh')).to exist
-    expect(file('bin/test.sh')).to be_mode(EXECUTABLE_MODE)
+    expect(file('bin/test.sh')).to be_executable
   end
 end
