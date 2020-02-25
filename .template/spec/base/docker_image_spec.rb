778bbeb10097e63ddea7917d2b91753e33fb3a8c
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-02-25 12:52:23 +0700

    Fix the server port spec

--- .template/spec/base/docker_image_spec.rb
+++ .template/spec/base/docker_image_spec.rb
@@ -1,5 +1,5 @@
 describe 'Docker image' do
-  xdescribe port(80) do
-    it { should be_listening.with('tcp') }
+  it 'is listening on port 80' do
+    expect(port(80)).to be_listening.with('tcp')
   end
 end
