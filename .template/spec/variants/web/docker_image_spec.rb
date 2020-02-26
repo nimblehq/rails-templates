b4b5f86336e598d345d4e72afe5e365facddf697
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-02-26 11:53:26 +0700

    Add docker image spec for web variant

---
+++ .template/spec/variants/web/docker_image_spec.rb
@@ -0,0 +1,9 @@
+describe 'Web variant - Docker image' do
+  it 'installs yarn package' do
+    expect(package('yarn')).to be_installed
+  end
+
+  it 'installs nodejs package' do
+    expect(package('nodejs')).to be_installed
+  end
+end
