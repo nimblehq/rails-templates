474923a1e4408b6eede9cd6a01dda53e324fed25
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-02-26 16:02:24 +0700

    Add comment on the wait time for the port spec

--- .template/spec/base/application_spec.rb
+++ .template/spec/base/application_spec.rb
@@ -1,5 +1,6 @@
 describe 'Application' do
   it 'is listening on port 80' do
+    # Config the wait time to 1 minute in case if the container has not fully started.
     wait(60).for do
       expect(port(80)).to be_listening.with('tcp')
     end
