ab9078ccb75acb7ccded846dca1d7a34748584b4
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-03-11 12:31:35 +0700

    Remove phraseapp, reorder arguments and rename variable

--- template.rb
+++ template.rb
@@ -3,7 +3,7 @@ require 'shellwords'
 # Variables
 APP_NAME = app_name
 # Transform the app name from slug to human-readable name e.g. nimble-web -> Nimble
-HUMAN_APP_NAME = app_name.split(/[-_]/).map(&:capitalize).join(' ').gsub(/ Web$/, '')
+APP_NAME_HUMANIZED = app_name.split(/[-_]/).map(&:capitalize).join(' ').gsub(/ Web$/, '')
 DOCKER_IMAGE = "nimblehq/#{APP_NAME}"
 RUBY_VERSION = '2.6.5'
 POSTGRES_VERSION = '12.1'
