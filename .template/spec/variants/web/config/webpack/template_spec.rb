970afd6a633f1c645238fe4cc006977931a333f3
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-03-13 18:19:16 +0700

    Fix rubocop and tests

--- .template/spec/variants/web/config/webpack/template_spec.rb
+++ .template/spec/variants/web/config/webpack/template_spec.rb
@@ -1,5 +1,5 @@
 describe 'Web variant - /config/webpack template' do
   it 'configures the webpack compilation in test environment to false' do
-    expect(file('config/webpacker.yml')).to contain("compile: false").from(/^test:/).to(/^production:/)
+    expect(file('config/webpacker.yml')).to contain("compile: false").from(/^test:$/).to(/^production:$/)
   end
 end
