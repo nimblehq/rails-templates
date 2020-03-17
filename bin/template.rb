bd0f07e498fe64d8ecd1ec115a5f42b64f71022a
Rossukhon Leagmongkol <rose.roko@gmail.com>
2020-03-17 15:26:36 +0700

    Add precompiling assets script

--- bin/template.rb
+++ bin/template.rb
@@ -2,3 +2,4 @@ copy_file 'bin/envsetup.sh', mode: :preserve
 copy_file 'bin/start.sh', mode: :preserve
 copy_file 'bin/test.sh', mode: :preserve
 copy_file 'bin/docker-prepare', mode: :preserve
+copy_file 'bin/docker-assets-precompile', mode: :preserve
