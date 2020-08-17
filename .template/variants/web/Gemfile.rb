6b977ac6429c144e53b705002d7203baa0d9f862
An Duong <duonghienan.dha@gmail.com>
2020-08-17 16:26:36 +0700

    [176] Upgrade WEbpacker

--- .template/variants/web/Gemfile.rb
+++ .template/variants/web/Gemfile.rb
@@ -8,7 +8,7 @@ insert_into_file 'Gemfile', after: %r{gem 'pundit'.*\n} do
   <<~EOT
 
     # Assets
-    gem 'webpacker', '4.0' # Transpile app-like JavaScript
+    gem 'webpacker', '~>5.2.0' # Transpile app-like JavaScript
     gem 'sass-rails' # SASS
   EOT
 end
