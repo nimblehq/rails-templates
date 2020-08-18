4e52ce99fc4b11ee0fcdbbfed2560639dc7388f7
An Duong <duonghienan.dha@gmail.com>
2020-08-18 11:50:18 +0700

    Adding the _

--- spec/codebase/codebase_spec.rb
+++ spec/codebase/codebase_spec.rb
@@ -17,7 +17,7 @@ describe 'Codebase', codebase: true do
     engine_paths = Dir[File.expand_path(Rails.root.join('engines', '*'))]
                    .select { |f| File.directory? f }
                    .map { |path| path.split('/').last }
-    invalid_engine_paths = engine_paths.reject { |path| path.start_with?("APP_NAME_HERE") }
+    invalid_engine_paths = engine_paths.reject { |path| path.start_with?("APP_NAME_HERE_") }
 
     expect(invalid_engine_paths).to be_empty
   end
