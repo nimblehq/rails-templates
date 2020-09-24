e9cdbf92ed635d5433de75f503c19a93cb2d917c
An Duong <andyduong1920@gmail.com>
2020-09-24 16:56:30 +0700

    Run Phrase app after bundle

--- .template/addons/phrase_app/template.rb
+++ .template/addons/phrase_app/template.rb
@@ -1,4 +1,6 @@
-use_source_path __dir__
-
-copy_file '.phraseapp.yml'
-apply 'spec/template.rb'
+after_bundle do
+  use_source_path __dir__
+  
+  copy_file '.phraseapp.yml'
+  apply 'spec/template.rb'
+end
