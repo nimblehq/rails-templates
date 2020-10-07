df20b777c9db44f8766a3058690297d44b0f41d5
An Duong <andyduong1920@gmail.com>
2020-10-07 14:21:20 +0700

    Adding unit test

--- .template/spec/base/config/environments/production_spec.rb
+++ .template/spec/base/config/environments/production_spec.rb
@@ -8,6 +8,14 @@ describe 'config/environments/production.rb' do
   it 'configures the mailer default url options' do
     expect(subject).to contain(mailer_default_url_config)
   end
+  
+  it 'adds the i18n configuration' do
+    expect(subject).to contain(i18n_config)
+  end
+  
+  it 'removes the config.i18n.fallbacks = true' do
+    expect(subject).not_to contain("config.i18n.fallbacks = true")
+  end
 
   it 'allows Rails serve static file' do
     expect(subject).to contain('config.public_file_server.enabled = true')
@@ -21,6 +29,14 @@ describe 'config/environments/production.rb' do
         host: ENV.fetch('MAILER_DEFAULT_HOST'),
         port: ENV.fetch('MAILER_DEFAULT_PORT')
       }
+    EOT
+  end
+  
+  def i18n_config
+    <<~EOT
+      config.i18n.available_locales = ENV.fetch('AVAILABLE_LOCALES').split(', ')
+      config.i18n.default_locale = ENV.fetch('DEFAULT_LOCALE')
+      config.i18n.fallbacks = ENV.fetch('FALLBACK_LOCALES').split(', ')
     EOT
   end
 end
