13bb9a4a6e39e60c50e8971dceccf5a1b4c70bd5
An Duong <andyduong1920@gmail.com>
2020-10-07 16:25:38 +0700

    Adding unit test

--- .template/addons/nginx/docker.rb
+++ .template/addons/nginx/docker.rb
@@ -13,11 +13,3 @@ end
 gsub_file 'Dockerfile', /PORT=80/ do
   'PORT=3000'
 end
-
-insert_into_file 'Dockerfile', after: %r(WORKDIR.+\n) do
-  <<-EOT
-
-# Nginx config
-COPY config/nginx/app.conf.template /etc/nginx/conf.d/default.conf
-  EOT
-end
---
+++ .template/spec/addons/nginx/template_spec.rb
@@ -0,0 +1,19 @@
+describe 'Nginx addon - template' do
+  it 'creates .config/nginx/app.conf.template file' do
+    expect(file('.config/nginx/app.conf.template')).to exist
+  end
+  
+  it 'disables Rails serve static file' do
+    expect(file('config/environments/production.rb')).to contain('config.public_file_server.enabled = false')
+  end
+  
+  it 'starts nginx in start.sh' do
+    expect(file('bin/start.sh')).to contain('nginx -c /etc/nginx/conf.d/default.conf')
+  end
+  
+  it 'adjusts Dockerfile to work with Nginx' do
+    expect(file('Dockerfile')).to contain('nginx').after('unzip')
+    expect(file('Dockerfile')).to contain('PORT=3000')
+    expect(file('Dockerfile')).to contain('COPY config/nginx/app.conf.template /etc/nginx/conf.d/default.conf')
+  end
+end
