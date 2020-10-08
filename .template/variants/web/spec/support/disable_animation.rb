a304b142f23625df56005fe1cebc976cdc74b819
An Duong <andyduong1920@gmail.com>
2020-10-08 11:20:44 +0700

    Fix rubocop-perfomance warning

--- .template/variants/web/spec/support/disable_animation.rb
+++ .template/variants/web/spec/support/disable_animation.rb
@@ -23,7 +23,7 @@ module Rack
     private
 
     def html?
-      @headers['Content-Type'] =~ /html/
+      @headers['Content-Type'].include?(/html/)
     end
 
     # rubocop:disable Metrics/MethodLength
