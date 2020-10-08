f8c667c25f1d910968a3c731b4697074165d50c5
An Duong <andyduong1920@gmail.com>
2020-10-08 13:23:45 +0700

    adding prefixes

--- spec/codebase/codebase_spec.rb
+++ spec/codebase/codebase_spec.rb
@@ -22,15 +22,13 @@ describe 'Codebase', codebase: true do
     expect(invalid_engine_paths).to be_empty
   end
 
-  context 'respond_to blocks' do
-    it 'does not contain respond_to blocks' do
-      find_results = `grep -r 'respond_to do' app/`
-      expect(find_results).to be_empty
-    end
+  it 'does not contain respond_to blocks' do
+    find_results = `grep -r 'respond_to do' app/`
+    expect(find_results).to be_empty
+  end
 
-    it 'does not contain format blocks' do
-      find_results = `grep -r 'format.json' app/`
-      expect(find_results).to be_empty
-    end
+  it 'does not contain format blocks' do
+    find_results = `grep -r 'format.json' app/`
+    expect(find_results).to be_empty
   end
 end
