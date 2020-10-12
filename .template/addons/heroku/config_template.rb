fb49d58544d911d81c69b8da4041687dfa85a460
Noor Ahmed <biswas.noorahmed@gmail.com>
2020-10-12 11:47:47 +0600

    [issue-218] Removed redundent static values

--- .template/addons/heroku/config_template.rb
+++ .template/addons/heroku/config_template.rb
@@ -1,7 +1,5 @@
 insert_into_file 'config/environments/production.rb', before: /%r{end\nend\Z}/ do
   <<~CODE
-    config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
     if ENV["RAILS_LOG_TO_STDOUT"].present?
       logger           = ActiveSupport::Logger.new(STDOUT)
       logger.formatter = config.log_formatter
--- .template/addons/heroku/env_variables.rb
+++ .template/addons/heroku/env_variables.rb
@@ -1,2 +1,1 @@
-echo 'RAILS_SERVE_STATIC_FILES=true' >> '.env' # default value
 echo 'RAILS_LOG_TO_STDOUT=true' >> '.env' # default value
