f68e6456a2b6146864b532dc2b134e2add458bfc
Noor Ahmed <biswas.noorahmed@gmail.com>
2020-10-14 11:51:41 +0600

    [issue-164] Moved generate command to devise/template.rb

--- .template/addons/devise/template.rb
+++ .template/addons/devise/template.rb
@@ -2,5 +2,5 @@ apply '.template/addons/devise/Gemfile.rb'
 apply '.template/addons/devise/spec_support.rb'
 
 after_bundle do
-  generate 'devise:install' if @add_devise
+  generate 'devise:install'
 end
