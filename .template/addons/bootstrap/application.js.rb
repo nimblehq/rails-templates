5b4b36138caabe882d9b12e7b2f12cbcbf1a1979
rafayet-monon <rafayet.monon@gmail.com>
2020-10-14 13:30:30 +0600

    Fix bootstrap import position.

--- .template/addons/bootstrap/application.js.rb
+++ .template/addons/bootstrap/application.js.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-insert_into_file 'app/javascript/packs/application.js', after: %r{import 'core-js/stable'.+\n} do
+insert_into_file 'app/javascript/packs/application.js', before: %r{import 'translations/translations'.+\n} do
   <<~JAVASCRIPT
     import 'vendor/bootstrap';
   JAVASCRIPT
