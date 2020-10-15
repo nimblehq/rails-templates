39ea1f87d09352c5bc8e5f1bda9b50ce16a6f3c0
An Duong <andyduong1920@gmail.com>
2020-10-15 11:16:07 +0700

    Remove unnecessary code

--- .template/variants/api/app/controllers/concerns/localization.rb
+++ .template/variants/api/app/controllers/concerns/localization.rb
@@ -17,11 +17,5 @@ module Localization
       
       nil
     end
-
-    private
-
-    def default_url_options
-      { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
-    end
   end
 end
