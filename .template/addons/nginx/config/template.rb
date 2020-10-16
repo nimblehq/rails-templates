0ed14edaf16592e3c2ef540ee681c5a560ef7f7e
An Duong <andyduong1920@gmail.com>
2020-10-16 22:00:50 +0700

    Minor refactor Nginx addon

--- .template/addons/nginx/config/template.rb
+++ .template/addons/nginx/config/template.rb
@@ -1,9 +1,5 @@
 use_source_path __dir__
 
-if WEB_VARIANT
-  apply 'config/environments/production.rb'
-  
-  template 'config/nginx/web/app.conf.template.tt', 'config/nginx/app.conf.template'
-else
-  template 'config/nginx/api/app.conf.template.tt', 'config/nginx/app.conf.template'
-end
+apply 'config/environments/production.rb' if WEB_VARIANT
+
+template 'config/nginx/app.conf.template.tt'
