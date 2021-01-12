91d7da0b468b7c0dcb9d459bd14b1e4e6034e10f
Hoang Mirs <hoang.mirs@gmail.com>
2021-01-12 14:22:34 +0700

    Make bootstrap variant modification not depend on translation import

--- .template/addons/bootstrap/application.js.rb
+++ .template/addons/bootstrap/application.js.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-insert_into_file 'app/javascript/packs/application.js', before: %r{import 'translations/translations'.+\n} do
+insert_into_file 'app/javascript/packs/application.js', after: %r{import "channels"\n} do
   <<~JAVASCRIPT
+
     import 'vendor/bootstrap';
   JAVASCRIPT
 end
