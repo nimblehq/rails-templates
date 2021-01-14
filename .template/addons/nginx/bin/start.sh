b10cf2458427d8a03708af6ddac372d2fb6a1862
An Duong <andyduong1920@gmail.com>
2021-01-14 10:31:20 +0700

    Start nginx on heroku

--- .template/addons/nginx/bin/start.sh
+++ .template/addons/nginx/bin/start.sh
@@ -1,7 +1,11 @@
 insert_into_file 'bin/start.sh', after: "fi\n" do
   <<-EOT
 
+sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf
+
 nginx -c /etc/nginx/conf.d/default.conf
 
   EOT
 end
+
+gsub_file('bin/start.sh', 'bundle exec rails s -p $PORT -b 0.0.0.0', 'bundle exec rails s -p 3000 -b 0.0.0.0')
