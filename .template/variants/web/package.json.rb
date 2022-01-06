60d2bbefd40e0a8030d90f2cc476f7df9e5ac95d
Rossukhon Leagmongkol <rose.roko@gmail.com>
2022-01-06 16:43:46 +0700

    Fix package.json config and remove deprecated turbolinks modification

--- .template/variants/web/package.json.rb
+++ .template/variants/web/package.json.rb
@@ -1,22 +1,21 @@
-insert_into_file 'package.json', after: %r{"private":.+\n} do
+package_json_file = 'package.json'
+
+run 'yarn init -yp'
+gsub_file package_json_file, /.*version.*/, ''
+gsub_file package_json_file, /.*main.*/, ''
+gsub_file package_json_file, /.*author.*/, ''
+gsub_file package_json_file, /.*license.*/, ''
+
+# Install dependencies
+run 'yarn add i18n-js@^3.8.0'
+run 'yarn add --dev @nimblehq/eslint-config-nimble@^2.2.1'
+
+# Setup scripts
+insert_into_file package_json_file, after: %r{"private":.+\n} do
   <<~EOT
     "scripts": {
       "lint": "eslint . --color",
       "lint:fix": "eslint . --color --fix"
     },
-  EOT
-end
-
-insert_into_file 'package.json', after: %r{"@rails/ujs":.+\n} do
-  <<~EOT
-    "i18n-js": "3.8.0",
-  EOT
-end
-
-insert_into_file 'package.json', before: %r{"version":.+\n} do
-  <<~EOT
-      "devDependencies": {
-        "@nimblehq/eslint-config-nimble": "2.1.1"
-      },
   EOT
 end
--- .template/variants/web/template.rb
+++ .template/variants/web/template.rb
@@ -18,8 +18,6 @@ def apply_web_variant!
   apply '.template/addons/bootstrap/template.rb' if yes? install_addon_prompt 'Bootstrap'
   apply '.template/addons/slim/template.rb' if yes? install_addon_prompt 'Slim Template Engine'
 
-  remove_turbolinks
-
   after_bundle do
     use_source_path __dir__
 
@@ -32,30 +30,6 @@ def apply_web_variant!
     apply 'config/webpack/template.rb'
     apply 'spec/template.rb'
   end
-end
-
-def remove_turbolinks
-  if File.exist?('app/views/layouts/application.html.erb')
-    gsub_file('app/views/layouts/application.html.erb', %r{, 'data-turbolinks-track': 'reload'}, '')
-  else
-    @template_errors.add <<~EOT
-      Cannot Remove turbolinks from `application.html.erb` file
-      Content: 'data-turbolinks-track': 'reload'
-    EOT
-  end
-
-  if File.exist?('app/javascript/packs/application.js')
-    gsub_file('app/javascript/packs/application.js', %r{import Turbolinks from "turbolinks"\n}, '')
-    gsub_file('app/javascript/packs/application.js', %r{Turbolinks.start\(\)\n}, '')
-  else
-    @template_errors.add <<~EOT
-      Cannot Remove turbolinks from `app/javascript/packs/application.js`
-      Content: import Turbolinks from 'turbolinks';
-      Content: Turbolinks.start();
-    EOT
-  end
-
-  gsub_file('package.json', %r{"turbolinks": .+\n}, '')
 end
 
 apply_web_variant!
