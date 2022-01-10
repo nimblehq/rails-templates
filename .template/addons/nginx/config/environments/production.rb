7562210f69a73758f5087a0099f9f0aff7d4a048
Rossukhon Leagmongkol <rose.roko@gmail.com>
2022-01-10 15:52:44 +0700

    Update nginx config

--- .template/addons/nginx/config/environments/production.rb
+++ .template/addons/nginx/config/environments/production.rb
@@ -1,11 +1,5 @@
 gsub_file(
   'config/environments/production.rb',
-  'config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?',
-  'config.public_file_server.enabled = false',
-)
-
-gsub_file(
-  'config/environments/production.rb',
-  'config.public_file_server.enabled = true',
+  %r{config.public_file_server.enabled.*},
   'config.public_file_server.enabled = false',
 )
--- .template/addons/nginx/config/template.rb
+++ .template/addons/nginx/config/template.rb
@@ -1,5 +1,5 @@
 use_source_path __dir__
 
-apply 'config/environments/production.rb' if WEB_VARIANT
+apply 'config/environments/production.rb'
 
 template 'config/nginx/app.conf.template.tt'
