50e86306f82bd5e97f9a7b920c87b2fcb9585115
Rossukhon Leagmongkol <rose.roko@gmail.com>
2022-01-10 16:33:04 +0700

    Refactor package.json

--- .template/variants/web/package.json.rb
+++ .template/variants/web/package.json.rb
@@ -1,18 +1,21 @@
-package_json_file = 'package.json'
-
-run 'yarn init -yp'
-gsub_file package_json_file, /.*version.*/, ''
-gsub_file package_json_file, /.*main.*/, ''
-gsub_file package_json_file, /.*author.*/, ''
-gsub_file package_json_file, /.*license.*/, ''
+# Create package.json file
+unless File.exist?('package.json')
+  create_file 'package.json',
+              <<~EOT
+                {
+                  "name": "#{APP_NAME}",
+                  "private": "true"
+                }
+              EOT
+end
 
 # Install dependencies
 run 'yarn add i18n-js@^3.8.0'
 run 'yarn add --dev @nimblehq/eslint-config-nimble@^2.2.1'
 
 # Setup scripts
-insert_into_file package_json_file, after: %r{"private":.+\n} do
-  <<~EOT
+insert_into_file 'package.json', after: %r{"private":.+\n} do
+  <<~EOT.indent(2)
     "scripts": {
       "lint": "eslint . --color",
       "lint:fix": "eslint . --color --fix"
