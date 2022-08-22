d65fdb7ff450bd09f3bcd7246f7a8e75b56e577b
Junan Chakma <junan.cs@gmail.com>
2022-08-22 18:36:24 +0700

    [gh-261] More refactoring

--- .template/addons/phrase/spec/codebase/codebase_spec.rb
+++ .template/addons/phrase/spec/codebase/codebase_spec.rb
@@ -10,13 +10,13 @@ insert_into_file 'spec/codebase/codebase_spec.rb', before: /end\Z/ do
                     .map { |path| path.split('.').first } # ["campaign", "article", "campaign"]
                     .uniq # ["campaign", "article"]
                     .sort # ["article", "campaign"]
-      phrase_app_pull_tags = YAML
+      phrase_pull_tags = YAML
                              .load_file(Rails.root.join('.phrase.yml'))
                              .dig('phrase', 'pull', 'targets')
                              .map { |target| target.dig('params', 'tags') }
                              .sort
 
-      expect(phrase_app_pull_tags).to eq locale_tags
+      expect(phrase_pull_tags).to eq locale_tags
     end
     # rubocop:enable RSpec/ExampleLength
   RUBY
