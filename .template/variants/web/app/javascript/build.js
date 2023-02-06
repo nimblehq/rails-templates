5f1a0c5a4e4c4cd258dc6612a445357649323357
Xavier MALPARTY <xavier@nimblehq.co>
2023-02-06 11:14:28 +0700

    [#380] Fix esbuild watch implementation for v0.17.*

--- .template/variants/web/app/javascript/build.js
+++ .template/variants/web/app/javascript/build.js
@@ -1,13 +1,17 @@
 const watch = process.argv.slice(2).includes('--watch');
 
-require('esbuild').build({
-  entryPoints: ['app/javascript/application.js'],
-  inject: ['app/javascript/global.js'],
-  bundle: true,
-  sourcemap: true,
-  watch: watch,
-  outdir: 'app/assets/builds',
-}).catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
+require('esbuild')
+  .context({
+    entryPoints: ['app/javascript/application.js'],
+    inject: ['app/javascript/global.js'],
+    bundle: true,
+    sourcemap: true,
+    outdir: 'app/assets/builds',
+  })
+  .then((ctx) => {
+    watch ? ctx.watch() : ctx.rebuild().then(() => process.exit(0));
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
