023ed000f2ba888b9ab8f274ed49f18fe0e35308
Andy Duong <andyduong1920@gmail.com>
2023-03-13 16:17:34 +0700

    [gh1183] Remove the request support

--- .template/addons/devise/spec_support.rb
+++ .template/addons/devise/spec_support.rb
@@ -4,6 +4,5 @@ file 'spec/support/devise.rb', <<~RUBY
   RSpec.configure do |config|
     config.include Devise::Test::ControllerHelpers, type: :controller
     config.include Devise::Test::ControllerHelpers, type: :view
-    config.include Devise::Test::IntegrationHelpers, type: :request
   end
 RUBY
