d668b7b2308fa4c957b54c4768302fd28fcbbebb
Xavier MALPARTY <xavier@nimblehq.co>
2023-03-24 14:53:29 +0700

    Fix test

--- .template/addons/phrase/spec/codebase/codebase_spec.rb
+++ .template/addons/phrase/spec/codebase/codebase_spec.rb
@@ -6,12 +6,18 @@ insert_into_file 'spec/codebase/codebase_spec.rb', before: /end\Z/ do
     # rubocop:disable RSpec/ExampleLength
     it 'does not offend Phrase Pull configuration' do
       locale_tags = Dir[File.expand_path(Rails.root.join('config', 'locales', '*.yml'))]
-                    .map { |path| path.split('/').last } # ["campaign.en.yml", "article.en.yml", "campaign.th.yml"]
-                    .map { |path| path.split('.').first } # ["campaign", "article", "campaign"]
-                    .uniq # ["campaign", "article"]
-                    .sort # ["article", "campaign"]
-      phrase_pull_tags = YAML
-                             .load_file(Rails.root.join('.phrase.yml'))
+                    # ['campaign.en.yml', 'article.en.yml', 'campaign.th.yml', 'en.yml']
+                    .map { |path| path.split('/').last }
+                    # ['campaign', 'article', 'campaign', 'en']
+                    .map { |path| path.split('.').first }
+                    # ['campaign', 'article', 'en']
+                    .uniq
+                    # ['article', 'campaign', 'en']
+                    .sort
+      # replace en with root as `en` is not a tag
+      locale_tags[locale_tags.index('en')] = 'root'
+
+      phrase_pull_tags = YAML.load_file(Rails.root.join('.phrase.yml'))
                              .dig('phrase', 'pull', 'targets')
                              .map { |target| target.dig('params', 'tags') }
                              .sort
