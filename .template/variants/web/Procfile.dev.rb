740b33ea0932e2cf3b22ea337e7630ac3d4681a6
Sang Huynh <sanghuynh20000@gmail.com>
2023-04-28 16:43:01 +0700

    [#384] Implementation

--- .template/variants/web/Procfile.dev.rb
+++ .template/variants/web/Procfile.dev.rb
@@ -4,5 +4,6 @@ append_to_file 'Procfile.dev' do
   <<~PROCFILE
     js: yarn build --watch
     css: yarn build:css --watch
+    postcss: yarn build:postcss --watch
   PROCFILE
 end
--- .template/variants/web/package.json.rb
+++ .template/variants/web/package.json.rb
@@ -11,6 +11,17 @@ unless File.exist?('package.json')
               JSON
 end
 
+unless File.exist?('postcss.config.js')
+  create_file 'postcss.config.js',
+              <<~POSTCSS
+                module.exports = {
+                  plugins: [
+                    require('autoprefixer'),
+                  ]
+                }
+              POSTCSS
+end
+
 # Install dependencies
 run 'yarn add i18n-js@3.8.0'
 run 'yarn add sass'
@@ -21,7 +32,7 @@ run 'yarn add --dev stylelint'
 run 'yarn add --dev @nimblehq/stylelint-config-nimble'
 # TODO: Check again after removing Webpacker, need to use version 8
 # https://github.com/bjankord/stylelint-config-sass-guidelines/issues/203#issuecomment-955620774
-run 'yarn add --dev postcss@8.4.5'
+run 'yarn add --dev postcss@8.4.5 postcss-cli autoprefixer'
 
 # Setup scripts
 run 'npm set-script eslint "eslint . --color"'
@@ -42,4 +53,9 @@ run %(npm set-script build "node app/javascript/build.js")
 run %(
   npm set-script build:css \
   "sass #{source_stylesheet} #{bundled_stylesheet} #{bundled_stylesheet_options.join(' ')}"
+)
+run %(npm set-script postcss "postcss public/assets/*.css --dir public/assets --config ./")
+run %(
+  npm set-script build:postcss \
+  "postcss app/assets/builds/*.css --dir app/assets/builds --config ./",
 )
