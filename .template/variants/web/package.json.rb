3b2c3d6de51cd2d5185ff2e6b7832e2d17d85547
Andy Duong <andyduong1920@gmail.com>
2023-07-03 13:28:06 +0700

    [#383] Fix rubocop

--- .template/variants/web/package.json.rb
+++ .template/variants/web/package.json.rb
@@ -52,7 +52,7 @@ production_bundled_stylesheet_options = bundled_stylesheet_base_options + [
 run %(npm set-script build "node app/javascript/build.js")
 run %(
   npm set-script build:css-production \
-  "sass #{source_stylesheet} #{bundled_stylesheet} #{(production_bundled_stylesheet_options).join(' ')}"
+  "sass #{source_stylesheet} #{bundled_stylesheet} #{production_bundled_stylesheet_options.join(' ')}"
 )
 run %(
   npm set-script build:css \
