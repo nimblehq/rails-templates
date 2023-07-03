5f7703362c5fe759a96fc1b74a3c4e9220b3a007
Andy Duong <andyduong1920@gmail.com>
2023-07-03 14:22:24 +0700

    [#383] Upgrade Node and Ruby dependencies

--- .template/variants/web/package.json.rb
+++ .template/variants/web/package.json.rb
@@ -23,21 +23,22 @@ unless File.exist?('postcss.config.js')
 end
 
 # Install dependencies
-run 'yarn add i18n-js@3.8.0'
+run 'yarn add i18n-js@4.2.3'
 run 'yarn add sass'
 run 'yarn add esbuild'
 run 'yarn add postcss postcss-cli autoprefixer'
 
-run 'yarn add --dev @nimblehq/eslint-config-nimble@2.2.1'
+run 'yarn add --dev @nimblehq/eslint-config-nimble@2.6.0'
 run 'yarn add --dev stylelint'
-run 'yarn add --dev @nimblehq/stylelint-config-nimble'
+run 'yarn add --dev @nimblehq/stylelint-config-nimble@1.0.2'
+
 # Setup scripts
-run 'npm set-script eslint "eslint . --color"'
-run 'npm set-script eslint:fix "eslint . --color --fix"'
-run 'npm set-script stylelint "stylelint **/*.scss --color"'
-run 'npm set-script stylelint:fix "stylelint **/*.scss --color --fix"'
-run 'npm set-script codebase "yarn eslint && yarn stylelint"'
-run 'npm set-script codebase:fix "yarn eslint:fix && yarn stylelint:fix"'
+run 'npm pkg set scripts.eslint="eslint . --color"'
+run 'npm pkg set scripts.eslint:fix="eslint . --color --fix"'
+run 'npm pkg set scripts.stylelint="stylelint **/*.scss --color"'
+run 'npm pkg set scripts.stylelint:fix="stylelint **/*.scss --color --fix"'
+run 'npm pkg set scripts.codebase="yarn eslint && yarn stylelint"'
+run 'npm pkg set scripts.codebase:fix="yarn eslint:fix && yarn stylelint:fix"'
 
 source_stylesheet = 'app/assets/stylesheets/application.scss'
 bundled_stylesheet = 'app/assets/builds/application.css'
@@ -49,17 +50,18 @@ production_bundled_stylesheet_options = bundled_stylesheet_base_options + [
   '--style=compressed'
 ]
 
-run %(npm set-script build "node app/javascript/build.js")
+run %(npm pkg set scripts.build="node app/javascript/build.js")
 run %(
-  npm set-script build:css-production \
-  "sass #{source_stylesheet} #{bundled_stylesheet} #{production_bundled_stylesheet_options.join(' ')}"
+  npm pkg set scripts.build:css-production="sass\
+  #{source_stylesheet} #{bundled_stylesheet} #{production_bundled_stylesheet_options.join(' ')}"
 )
 run %(
-  npm set-script build:css \
-  "sass #{source_stylesheet} #{bundled_stylesheet} #{bundled_stylesheet_base_options.join(' ')}"
+  npm pkg set scripts.build:css="sass\
+  #{source_stylesheet} #{bundled_stylesheet} #{bundled_stylesheet_base_options.join(' ')}"
 )
-run %(npm set-script postcss "postcss public/assets/*.css --dir public/assets --config ./")
+run %(npm pkg set scripts.postcss="postcss\
+  public/assets/*.css --dir public/assets --config ./")
 run %(
-  npm set-script build:postcss \
-  "postcss app/assets/builds/*.css --dir app/assets/builds --config ./",
+  npm pkg set scripts.build:postcss="postcss\
+  app/assets/builds/*.css --dir app/assets/builds --config ./",
 )
