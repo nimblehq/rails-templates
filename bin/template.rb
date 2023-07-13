7f0c393898cf72f51bda48159325694574c83243
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-07-13 16:24:25 +0700

    Remove the ./bin/dev.sh script in favor of the makefile (#443)
    * [#410] Remove bin/dev.sh and start foreman from makefile
    
    * [#410] Remove test related to bin/dev file existence
    
    * [#410] Remove instructions about copying bin/dev
    
    * [#410] Rename phrase add-on spec's template file

--- bin/template.rb
+++ bin/template.rb
@@ -5,6 +5,5 @@ copy_file 'bin/envteardown.sh', mode: :preserve
 copy_file 'bin/start.sh', mode: :preserve
 copy_file 'bin/test.sh', mode: :preserve
 copy_file 'bin/worker.sh', mode: :preserve
-copy_file 'bin/dev', mode: :preserve
 copy_file 'bin/docker-prepare', mode: :preserve
 copy_file 'bin/docker-assets-precompile', mode: :preserve
