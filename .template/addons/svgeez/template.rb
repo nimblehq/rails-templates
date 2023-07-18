0cd58381c972d621166233a9b1c0c243fa815e20
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-07-18 16:54:48 +0700

    [#425] Add svg helper for svgeez addon

--- .template/addons/svgeez/template.rb
+++ .template/addons/svgeez/template.rb
@@ -3,3 +3,4 @@
 apply '.template/addons/svgeez/Gemfile.rb'
 apply '.template/addons/svgeez/bin/template.rb'
 apply '.template/addons/svgeez/.github/wiki/template.rb'
+apply '.template/addons/svgeez/helpers/template.rb'
