142e347cab6637f58b2a485e4350a27407da527b
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-07-18 17:20:14 +0700

    [#425] Remove gemfile spec for svgeez

--- .template/spec/addons/variants/web/svgeez/gemfile_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-describe 'Svgeez Addon - Gemfile' do
-  subject { file('Gemfile') }
-
-  it 'adds svgeez gem' do
-    expect(subject).to contain('svgeez')
-  end
-end
