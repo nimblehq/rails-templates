3b2dd797edfa8d0efad4e54a6c458f178e1c859a
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-07-24 16:49:21 +0700

    [#425] Add inline_svg gem under production group

--- .template/addons/svgeez/Gemfile.rb
+++ .template/addons/svgeez/Gemfile.rb
@@ -3,9 +3,13 @@
 # SVG Sprite
 insert_into_file 'Gemfile', after: /gem 'danger'.*\n/ do
   <<~RUBY
-
-    # Svgeez
     gem 'svgeez' # Gem for generating an SVG sprite from a folder of SVG icons.
+  RUBY
+end
+
+# SVG Sprite
+insert_into_file 'Gemfile', after: /gem 'bcrypt'.*\n/ do
+  <<~RUBY
     gem 'inline_svg' # Use Inline SVG for styling SVG with CSS
   RUBY
 end
