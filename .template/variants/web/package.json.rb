63a705f98d2db0e7e3cbcfa3b30a0149ce64a139
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-07-24 13:47:14 +0700

    [#421] Use eslint config core yarn packange instead of eslint config

--- .template/variants/web/package.json.rb
+++ .template/variants/web/package.json.rb
@@ -30,7 +30,7 @@ run 'yarn add postcss postcss-cli autoprefixer'
 
 run 'yarn add --dev eslint'
 run 'yarn add --dev stylelint'
-run 'yarn add --dev @nimblehq/eslint-config-nimble@2.6.0'
+run 'yarn add --dev @nimblehq/eslint-config-nimble-core@2.6.0'
 run 'yarn add --dev @nimblehq/stylelint-config-nimble@1.0.3'
 
 # Setup scripts
