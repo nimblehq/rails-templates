de708915001d8bbaeb39530a662978dd917b24f8
Sang Huynh <sanghuynh20000@gmail.com>
2023-08-04 14:42:26 +0700

    [#417] Update files by variant

--- bin/template.rb
+++ bin/template.rb
@@ -6,4 +6,7 @@ copy_file 'bin/start.sh', mode: :preserve
 copy_file 'bin/test.sh', mode: :preserve
 copy_file 'bin/worker.sh', mode: :preserve
 copy_file 'bin/docker-prepare', mode: :preserve
+
+if WEB_VARIANT
 copy_file 'bin/docker-assets-precompile', mode: :preserve
+end
