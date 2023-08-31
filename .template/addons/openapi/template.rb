88df4ced0e50fe8b42f74052e035189cdcd32712
Sang Huynh <sanghuynh20000@gmail.com>
2023-08-31 15:23:59 +0700

    [#446] Add endpoint and template html

--- .template/addons/openapi/template.rb
+++ .template/addons/openapi/template.rb
@@ -7,3 +7,15 @@ directory '.github' if @install_github_action
 apply '.gitignore.rb'
 apply 'package.json.rb'
 copy_file '.spectral.yml'
+copy_file 'public/stoplight.html'
+
+after_bundle do
+  run 'yarn build:docs'
+
+  insert_into_file 'config/routes.rb', before: /^end/ do
+    <<~RUBY.indent(2)
+
+      get 'api/docs/openapi', to: redirect('/stoplight.html')
+    RUBY
+  end
+end
