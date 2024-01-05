f9dc30a92bbdada9af7c79fd3716556dd3a850f6
goose <purification1997@gmail.com>
2024-01-05 14:52:20 +0700

    [#494] Fix failing CI

--- .template/addons/custom_cops/template.rb
+++ .template/addons/custom_cops/template.rb
@@ -2,5 +2,4 @@
 
 require 'fileutils'
 
-FileUtils.mkdir_p 'lib/custom_cops'
-copy_file 'custom_cops/required_inverse_of_relations.rb', 'lib/custom_cops/required_inverse_of_relations.rb'
+copy_file 'custom_cops/required_inverse_of_relations.rb', mode: :preserve
