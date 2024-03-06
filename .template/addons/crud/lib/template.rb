6f7bf333f8969005831bac4d72b03a39e38c872d
khangbui22 <khangbui@umass.edu>
2024-03-06 21:29:57 +0700

    [#492] Fix rubocop issue

--- .template/addons/crud/lib/template.rb
+++ .template/addons/crud/lib/template.rb
@@ -6,7 +6,7 @@ def copy_template_files
 end
 
 def copy_non_tt_files
-  # The reason we are excluding .tt files is because 
+  # The reason we are excluding .tt files is because
   # this directory method will actually apply the template and remove the .tt extension in the destination
   directory 'lib/templates', exclude_pattern: /.*\.tt$/
 end
@@ -15,8 +15,8 @@ def copy_tt_files
   src_path = File.join(__dir__, 'templates')
   dst_path = 'lib/templates'
 
-  Dir.glob("#{src_path}/**/*.tt").sort.each do |file_source|
-    relative_path = file_source.gsub("#{src_path}/", "")
+  Dir.glob("#{src_path}/**/*.tt").each do |file_source|
+    relative_path = file_source.gsub("#{src_path}/", '')
 
     copy_file(file_source, File.join(dst_path, relative_path))
   end
