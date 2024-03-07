01f07e5f4575cb99f64b43b51a0e4bb102224ce1
khangbui22 <khangbui@umass.edu>
2024-03-07 17:36:38 +0700

    [#492] Fix rubocop error

--- .template/addons/crud/lib/template.rb
+++ .template/addons/crud/lib/template.rb
@@ -10,11 +10,11 @@ def ignore_tt
   # NOTE: change template extension so it would skip
   #       `when /#{TEMPLATE_EXTNAME}$/` condition and
   #        fallback to default `copy_file`
-  Thor::TEMPLATE_EXTNAME.concat "_no_match" # => .tt_no_match
+  Thor::TEMPLATE_EXTNAME.concat '_no_match' # => .tt_no_match
   yield
 ensure
   # NOTE: make sure to undo our dirty work after the block
-  Thor::TEMPLATE_EXTNAME.chomp! "_no_match" # => .tt
+  Thor::TEMPLATE_EXTNAME.chomp! '_no_match' # => .tt
 end
 
 def directory_with_tt(...)
