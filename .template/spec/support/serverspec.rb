405bf9d4c3c79df678325b3035cf9e3fe551720d
Sang Huynh Thanh <sanghuynh20000@gmail.com>
2024-03-13 14:17:17 +0700

    Update container name
    Update container name
    
    Update container name

--- .template/spec/support/serverspec.rb
+++ .template/spec/support/serverspec.rb
@@ -2,23 +2,13 @@
 
 require 'serverspec'
 require 'docker-api'
-require 'logger'
 
 module ServerSpecHelpers
   # Prebuild and run docker image before running the test
   # Because the docker api does not support docker compose
   def self.test_container
-    logger = Logger.new($stdout)
-    logger.level = Logger::WARN
-
-    Docker::Container.all(:all => true).each do |container|
-      logger.warn "Container: #{container.id} #{container.info['Names']}"
-    end
-
     container_name = "rails-template-test-run"
     container_id = `docker ps -qf "name=#{container_name}"`
-    logger.warn "Container name: #{container_name}"
-    logger.warn "Container ID: #{container_id}"
 
     Docker::Container.get(container_id.strip)
   end
